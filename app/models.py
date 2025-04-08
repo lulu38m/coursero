a7fba8d5e2b6999c2b7ae2ba0ef28139f5c7e225
lucas <lucas.moreau@supinfo.com>
2025-04-08 10:30:19 +0200

    Update database URI in configuration to use root user

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
@@ -13,3 +14,4 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"<User {self.email}>"
+
